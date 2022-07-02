0e9e4542cc8ff843566868d78e84a308efdfda78
pinchitos09 <58122707+pinchitos09@users.noreply.github.com>
2022-07-02 11:32:13 -0500

    vc2
    assad

--- Node.java
+++ Node.java
@@ -1,6 +1,7 @@
 
 public class Node {
     public static void main(String[] args) {
-        System.out.println("hola");
+        System.out.println("hola ");
+          System.out.println("hola x2 ");
     }
 }
